43963ccfcc7eee91d881e56d41abff61e2922303
Junikarp <zieunpk0924@naver.com>
2025-09-24 06:54:38 +0900

    프로젝트 생성

---
+++ src/main/java/com/junikarp/qbank/AwsQbankApplication.java
@@ -0,0 +1,13 @@
+package com.junikarp.qbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsQbankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsQbankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/junikarp/qbank/AwsQbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junikarp.qbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsQbankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
