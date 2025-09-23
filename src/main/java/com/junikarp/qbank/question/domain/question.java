7dca9c4551a5f1105f3d67f35ca4189c0b871651
Junikarp <zieunpk0924@naver.com>
2025-09-24 07:21:19 +0900

    문제 도메인 엔티티 추가

---
+++ src/main/java/com/junikarp/qbank/question/domain/question.java
@@ -0,0 +1,10 @@
+package com.junikarp.qbank.question.domain;
+
+import java.util.List;
+
+public class question {
+    private Long id;
+    private String question;
+    private String explanation;
+    private List<String> options;
+}
