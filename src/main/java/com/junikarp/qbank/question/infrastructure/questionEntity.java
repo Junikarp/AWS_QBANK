f50e9fd4cf4cc7711d45fbccccd7ebf20aec2a66
Junikarp <zieunpk0924@naver.com>
2025-09-24 07:36:45 +0900

    문제 영속성 객체 추가

---
+++ src/main/java/com/junikarp/qbank/question/infrastructure/questionEntity.java
@@ -0,0 +1,23 @@
+package com.junikarp.qbank.question.infrastructure;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class questionEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "question", columnDefinition = "TEXT")
+    private String question;
+
+    @Column(name = "explanation", columnDefinition = "TEXT")
+    private String explanation;
+
+    @Column(name = "options")
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<OptionEntity> options;
+}
