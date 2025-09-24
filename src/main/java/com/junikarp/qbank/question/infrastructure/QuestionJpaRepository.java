81058ebb6e03f6d9d400be645c4d6a389feaadd4
Junikarp <zieunpk0924@naver.com>
2025-09-25 05:23:39 +0900

    전체 문제 가져오기 메서드 추가

--- src/main/java/com/junikarp/qbank/question/infrastructure/QuestionJpaRepository.java
+++ src/main/java/com/junikarp/qbank/question/infrastructure/QuestionJpaRepository.java
@@ -1,11 +1,14 @@
 package com.junikarp.qbank.question.infrastructure;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface QuestionJpaRepository extends JpaRepository<QuestionEntity, Long> {
 
     Optional<QuestionEntity> findById();
 
+    List<QuestionEntity> findAll();
 }
--- src/main/java/com/junikarp/qbank/question/infrastructure/QuestionRepositoryImpl.java
+++ src/main/java/com/junikarp/qbank/question/infrastructure/QuestionRepositoryImpl.java
@@ -1,10 +1,20 @@
 package com.junikarp.qbank.question.infrastructure;
 
+import com.junikarp.qbank.question.domain.Question;
 import com.junikarp.qbank.question.service.port.QuestionRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 public class QuestionRepositoryImpl implements QuestionRepository {
 
     private QuestionJpaRepository questionJpaRepository;
+
+    public List<Question> findAll() {
+        return questionJpaRepository.findAll().stream()
+                .map(QuestionEntity::to)
+                .collect(Collectors.toList());
+    }
 }
