536179e763c0a4cf4e315f7aa8fecb2499167f3a
Junikarp <zieunpk0924@naver.com>
2025-09-24 16:42:48 +0900

    도메인 엔티티와 영속성 객체 간 변환 메서드 추가

--- src/main/java/com/junikarp/qbank/option/infrastructure/OptionEntity.java
+++ src/main/java/com/junikarp/qbank/option/infrastructure/OptionEntity.java
@@ -1,8 +1,15 @@
 package com.junikarp.qbank.option.infrastructure;
 
+import com.junikarp.qbank.option.domain.Option;
 import com.junikarp.qbank.question.infrastructure.QuestionEntity;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Table(name = "option")
+@Getter
+@Setter
 public class OptionEntity {
 
     @Id
@@ -18,9 +25,27 @@ public class OptionEntity {
     @Column(name = "is_correct")
     private Boolean isCorrect;
 
-    @Column(name = "question_id")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "question_id", referencedColumnName = "id")
     private QuestionEntity questionEntity;
 
+    public static OptionEntity from(Option option) {
+        OptionEntity optionEntity = new OptionEntity();
+        optionEntity.id = option.getId();
+        optionEntity.number = option.getNumber();
+        optionEntity.text = option.getText();
+        optionEntity.isCorrect = option.getIsCorrect();
+
+        return optionEntity;
+    }
+
+    public Option to() {
+        return Option.builder()
+                .id(id)
+                .number(number)
+                .text(text)
+                .isCorrect(isCorrect)
+                .questionId(questionEntity.getId())
+                .build();
+    }
 }
--- src/main/java/com/junikarp/qbank/question/infrastructure/QuestionEntity.java
+++ src/main/java/com/junikarp/qbank/question/infrastructure/QuestionEntity.java
@@ -1,10 +1,19 @@
 package com.junikarp.qbank.question.infrastructure;
 
+import com.junikarp.qbank.option.domain.Option;
+import com.junikarp.qbank.option.infrastructure.OptionEntity;
+import com.junikarp.qbank.question.domain.Question;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
+@Table(name = "question")
+@Getter
+@Setter
 public class QuestionEntity {
 
     @Id
@@ -17,7 +26,34 @@ public class QuestionEntity {
     @Column(name = "explanation", columnDefinition = "TEXT")
     private String explanation;
 
-    @Column(name = "options")
-    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "questionEntity", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<OptionEntity> options;
+
+
+    public static QuestionEntity from(Question question) {
+        QuestionEntity questionEntity = new QuestionEntity();
+        questionEntity.id = question.getId();
+        questionEntity.question = question.getQuestion();
+        questionEntity.explanation = question.getExplanation();
+        questionEntity.options = question.getOptions().stream()
+                .map(option -> {
+                    OptionEntity optionEntity = OptionEntity.from(option);
+                    optionEntity.setQuestionEntity(questionEntity);
+                    return optionEntity;
+                })
+                .collect(Collectors.toList());
+
+        return questionEntity;
+    }
+
+    public Question to() {
+        return Question.builder()
+                .id(id)
+                .question(question)
+                .explanation(explanation)
+                .options(options.stream()
+                        .map(OptionEntity::to)
+                        .collect(Collectors.toList()))
+                .build();
+    }
 }
