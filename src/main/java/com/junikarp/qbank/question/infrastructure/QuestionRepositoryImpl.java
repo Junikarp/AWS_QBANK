a15e79887a342d780b93229ea62d4acd8d73b19a
Junikarp <zieunpk0924@naver.com>
2025-09-25 19:11:45 +0900

    문제 도메인 repository 메서드 추가

--- src/main/java/com/junikarp/qbank/question/infrastructure/QuestionRepositoryImpl.java
+++ src/main/java/com/junikarp/qbank/question/infrastructure/QuestionRepositoryImpl.java
@@ -2,12 +2,15 @@ package com.junikarp.qbank.question.infrastructure;
 
 import com.junikarp.qbank.question.domain.Question;
 import com.junikarp.qbank.question.service.port.QuestionRepository;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Repository
+@RequiredArgsConstructor
 public class QuestionRepositoryImpl implements QuestionRepository {
 
     private QuestionJpaRepository questionJpaRepository;
@@ -16,5 +19,10 @@ public class QuestionRepositoryImpl implements QuestionRepository {
         return questionJpaRepository.findAll().stream()
                 .map(QuestionEntity::to)
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public Question save(Question question) {
+        return questionJpaRepository.save(QuestionEntity.from(question)).to();
     }
 }
--- src/main/java/com/junikarp/qbank/question/service/port/QuestionRepository.java
+++ src/main/java/com/junikarp/qbank/question/service/port/QuestionRepository.java
@@ -1,4 +1,13 @@
 package com.junikarp.qbank.question.service.port;
 
+import com.junikarp.qbank.question.domain.Question;
+import com.junikarp.qbank.question.infrastructure.QuestionEntity;
+
+import java.util.List;
+
 public interface QuestionRepository {
+
+    Question save(Question question);
+
+    List<Question> findAll();
 }
