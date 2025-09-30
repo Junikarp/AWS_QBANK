23e11b26a547b9362f18b6a960ed125ba8f245df
Junikarp <zieunpk0924@naver.com>
2025-09-30 14:05:24 +0900

    bookmark response 객체 구현

---
+++ src/main/java/com/junikarp/qbank/bookmark/controller/response/BookmarkQuestionListResponse.java
@@ -0,0 +1,25 @@
+package com.junikarp.qbank.bookmark.controller.response;
+
+import com.junikarp.qbank.question.controller.response.QuestionResponse;
+import com.junikarp.qbank.question.domain.Question;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class BookmarkQuestionListResponse {
+
+    private List<QuestionResponse> list;
+
+    public static BookmarkQuestionListResponse from (List<Question> questions) {
+        List<QuestionResponse> list = questions.stream()
+                .map(QuestionResponse::from)
+                .toList();
+
+        return BookmarkQuestionListResponse.builder()
+                .list(list)
+                .build();
+    }
+}
---
+++ src/main/java/com/junikarp/qbank/bookmark/controller/response/BookmarkResponse.java
@@ -0,0 +1,21 @@
+package com.junikarp.qbank.bookmark.controller.response;
+
+import com.junikarp.qbank.bookmark.domain.Bookmark;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class BookmarkResponse {
+    Long id;
+    Long userId;
+    Long questionId;
+
+    public static BookmarkResponse from(Bookmark bookmark) {
+        return BookmarkResponse.builder()
+                .id(bookmark.getId())
+                .userId(bookmark.getUserId())
+                .questionId(bookmark.getQuestionId())
+                .build();
+    }
+}
