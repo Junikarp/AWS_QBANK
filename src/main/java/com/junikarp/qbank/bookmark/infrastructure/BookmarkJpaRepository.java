6e8a9978c673c07ab89f2bc558b5ffcf187407ac
Junikarp <zieunpk0924@naver.com>
2025-10-03 06:50:04 +0900

    주석 추가

--- src/main/java/com/junikarp/qbank/bookmark/infrastructure/BookmarkJpaRepository.java
+++ src/main/java/com/junikarp/qbank/bookmark/infrastructure/BookmarkJpaRepository.java
@@ -1,14 +1,12 @@
 package com.junikarp.qbank.bookmark.infrastructure;
 
-import com.junikarp.qbank.bookmark.domain.Bookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface BookmarkJpaRepository extends JpaRepository<BookmarkEntity, Long> {
 
     List<BookmarkEntity> findByUserEntityId(Long userId);
-
+    // 유저 아이디와 문제 아이디로 북마크 내역 삭제
     void deleteByUserEntityIdAndQuestionEntityId(Long userId, Long questionId);
 }
