f5b8a85d342074ca5c3b68552baa363706bad593
Junikarp <zieunpk0924@naver.com>
2025-10-03 01:13:03 +0900

    BookmarkRepository test 추가

--- src/test/java/com/junikarp/qbank/bookmark/repository/BookmarkRepositoryTest.java
+++ src/test/java/com/junikarp/qbank/bookmark/repository/BookmarkRepositoryTest.java
@@ -1,61 +1,81 @@
 package com.junikarp.qbank.bookmark.repository;
 
+import com.junikarp.qbank.bookmark.domain.Bookmark;
 import com.junikarp.qbank.bookmark.infrastructure.BookmarkEntity;
 import com.junikarp.qbank.bookmark.infrastructure.BookmarkJpaRepository;
+import com.junikarp.qbank.bookmark.infrastructure.BookmarkRepositoryImpl;
+import com.junikarp.qbank.choice.infrastructure.ChoiceEntity;
+import com.junikarp.qbank.mock.TestFixtures;
+import com.junikarp.qbank.question.infrastructure.QuestionEntity;
+import com.junikarp.qbank.user.infrastructure.UserEntity;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlGroup;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-@ExtendWith(SpringExtension.class)
-@DataJpaTest
-@TestPropertySource("classpath:/test-application.properties")
-@SqlGroup({
-        @Sql(value = "/sql/bookmark-repository-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
-        @Sql(value = "/sql/all-data-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-})
+@ExtendWith(MockitoExtension.class)
 public class BookmarkRepositoryTest {
 
-    @Autowired
+    @InjectMocks
+    private BookmarkRepositoryImpl BookmarkRepositoryImpl;
+    @Mock
     private BookmarkJpaRepository bookmarkJpaRepository;
 
+    TestFixtures testFixtures = new TestFixtures();
+
     @Test
-    void 유저_아이디로_북마크_리스트를_가져올_수_있다() {
+    void save_메서드로_북마크를_저장할_수_있다() {
         //given
-        Long userId = 1L;
+        Bookmark bookmark = Bookmark.builder()
+                .id(1L)
+                .userId(1L)
+                .questionId(1L)
+                .build();
+
+        List<ChoiceEntity> choices1 = List.of(
+                testFixtures.createChoiceEntity(1L, "A번", "A번 보기입니다.", false),
+                testFixtures.createChoiceEntity(2L, "B번", "B번 보기입니다.", true)
+        );
+
+        UserEntity userEntity = testFixtures.createUserEntity();
+        QuestionEntity questionEntity = testFixtures.createQuestionEntity(1L, "1번 문제입니다", "1번 문제 해설입니다", choices1);
+
+        BookmarkEntity bookmarkEntity = BookmarkEntity.from(bookmark);
+        bookmarkEntity.setQuestionEntity(questionEntity);
+        bookmarkEntity.setUserEntity(userEntity);
 
         //when
-        List<BookmarkEntity> result = bookmarkJpaRepository.findByUserEntityId(userId);
+        when(bookmarkJpaRepository.save(any(BookmarkEntity.class))).thenReturn(bookmarkEntity);
+        Bookmark result = BookmarkRepositoryImpl.save(bookmark);
 
         //then
-        assertThat(result.size()).isEqualTo(2);
-        assertThat(result.get(0).getUserEntity().getId()).isEqualTo(1);
-        assertThat(result.get(0).getQuestionEntity().getId()).isEqualTo(1);
-        assertThat(result.get(1).getQuestionEntity().getId()).isEqualTo(3);
+        assertThat(result.getId()).isEqualTo(1L);
+        assertThat(result.getUserId()).isEqualTo(1L);
+        assertThat(result.getQuestionId()).isEqualTo(1L);
+
+        verify(bookmarkJpaRepository).save(any(BookmarkEntity.class));
     }
 
-    @Test
-    void 문제_아이디와_유저_아이디가_일치하는_북마크를_삭제할_수_있다(){
-        //given
-        Long userId = 1L;
-        Long questionId = 1L;
+/* TODO
+    @Override
+    public List<Bookmark> findByUserId(Long userId) {
+        List<BookmarkEntity> list = bookmarkJpaRepository.findByUserEntityId(userId);
+        return list.stream()
+                .map(BookmarkEntity::to)
+                .collect(Collectors.toList());
+    }
 
-        //when
+    @Override
+    public void delete(Long userId, Long questionId) {
         bookmarkJpaRepository.deleteByUserEntityIdAndQuestionEntityId(userId, questionId);
-
-        //then
-        List<BookmarkEntity> result = bookmarkJpaRepository.findByUserEntityId(userId);
-
-        assertThat(result.size()).isEqualTo(1);
-        assertThat(result.get(0).getQuestionEntity().getId()).isEqualTo(3);
     }
-
+*/
 }
