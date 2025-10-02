19a4708080ffd94e3fcde9f4d67cf90f1c6b87ad
Junikarp <zieunpk0924@naver.com>
2025-10-02 20:38:51 +0900

    북마크 문제 찾기 메서드 추가

--- src/main/java/com/junikarp/qbank/question/service/port/QuestionRepository.java
+++ src/main/java/com/junikarp/qbank/question/service/port/QuestionRepository.java
@@ -2,6 +2,7 @@ package com.junikarp.qbank.question.service.port;
 
 import com.junikarp.qbank.question.domain.Question;
 import com.junikarp.qbank.question.infrastructure.QuestionEntity;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,4 +11,7 @@ public interface QuestionRepository {
     Question save(Question question);
 
     List<Question> findAll();
+
+    List<Question> findBookmarkedQuestionsByUserId(Long userId);
+
 }
