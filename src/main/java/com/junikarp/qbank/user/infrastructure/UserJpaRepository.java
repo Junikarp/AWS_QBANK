c014625d372a372f9ecf26cf25e1bac96f706595
Junikarp <zieunpk0924@naver.com>
2025-10-03 06:18:31 +0900

    UserJpaRepository 파일 추가

---
+++ src/main/java/com/junikarp/qbank/user/infrastructure/UserJpaRepository.java
@@ -0,0 +1,6 @@
+package com.junikarp.qbank.user.infrastructure;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserJpaRepository extends JpaRepository<UserEntity,Long> {
+}
