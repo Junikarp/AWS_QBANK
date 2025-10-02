e51607e204638d01e7b3d93b3b24a53d286bdc49
Junikarp <zieunpk0924@naver.com>
2025-10-03 00:24:39 +0900

    UserEntity @Setter 추가

--- src/main/java/com/junikarp/qbank/user/infrastructure/UserEntity.java
+++ src/main/java/com/junikarp/qbank/user/infrastructure/UserEntity.java
@@ -8,12 +8,14 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
+@Setter
 @Table(name = "users")
 public class UserEntity {
 
