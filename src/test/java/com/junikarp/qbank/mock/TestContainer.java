5764ca52edc34902ee832a1d6335b4c14c4e0bbc
Junikarp <zieunpk0924@naver.com>
2025-10-06 07:29:18 +0900

    TestContainer 필요한 필드 선언 및 초기화

--- src/test/java/com/junikarp/qbank/mock/TestContainer.java
+++ src/test/java/com/junikarp/qbank/mock/TestContainer.java
@@ -1,34 +1,51 @@
 package com.junikarp.qbank.mock;
 
+import com.junikarp.qbank.bookmark.controller.BookmarkController;
+import com.junikarp.qbank.bookmark.controller.port.BookmarkService;
 import com.junikarp.qbank.bookmark.infrastructure.BookmarkRepositoryImpl;
+import com.junikarp.qbank.bookmark.service.BookmarkServiceImpl;
 import com.junikarp.qbank.bookmark.service.port.BookmarkRepository;
 import com.junikarp.qbank.question.controller.QuestionController;
 import com.junikarp.qbank.question.controller.port.QuestionService;
 import com.junikarp.qbank.question.controller.port.QuestionShuffler;
 import com.junikarp.qbank.question.service.QuestionServiceImpl;
 import com.junikarp.qbank.question.service.port.QuestionRepository;
+import com.junikarp.qbank.user.service.port.UserRepository;
 import lombok.Builder;
 
 public class TestContainer {
 
-    public final QuestionRepository questionRepository;
+    public final FakeQuestionRepository questionRepository;
     public final QuestionService questionService;
     public final QuestionController questionController;
     public final QuestionShuffler questionShuffler;
-    public final BookmarkRepository bookmarkRepository;
+
+    public final FakeBookmarkRepository bookmarkRepository;
+    public final BookmarkService bookmarkService;
+    public final BookmarkController bookmarkController;
+
+    public final FakeUserRepository userRepository;
 
     @Builder
     public TestContainer() {
-        FakeBookmarkRepository fakeBookmarkRepository = new FakeBookmarkRepository();
-        this.bookmarkRepository = fakeBookmarkRepository;
+        this.userRepository = new FakeUserRepository();
+        this.bookmarkRepository = new FakeBookmarkRepository();
         this.questionShuffler = new QuestionShufflerTest();
-        this.questionRepository = new FakeQuestionRepository(fakeBookmarkRepository);
+        this.questionRepository = new FakeQuestionRepository(bookmarkRepository);
         this.questionService = QuestionServiceImpl.builder()
                 .questionRepository(this.questionRepository)
                 .questionShuffler(this.questionShuffler)
                 .build();
         this.questionController = QuestionController.builder()
                 .questionService(this.questionService)
+                .build();
+        this.bookmarkService = BookmarkServiceImpl.builder()
+                .bookmarkRepository(this.bookmarkRepository)
+                .questionRepository(this.questionRepository)
+                .userRepository(this.userRepository)
+                .build();
+        this.bookmarkController = BookmarkController.builder()
+                .bookmarkService(this.bookmarkService)
                 .build();
     }
 }
